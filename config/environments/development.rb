f92caa770f215270695614f66e071e0ea279d6b1
Alan Salganik <aasalganik1@gmail.com>
2015-09-15 13:09:49 -0500

    fixing social share

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,13 +28,14 @@ Rails.application.configure do
   config.assets.debug = true
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
-    domain: "gmail.com",
+    domain: Rails.application.secrets.domain_name,
     authentication: "plain",
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
   }
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
